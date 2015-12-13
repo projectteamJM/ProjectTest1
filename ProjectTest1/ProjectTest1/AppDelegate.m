e787a19bbab62e1aaac04ba8077fd593afe381dc
xiaohan <917532427@qq.com>
2015-12-13 15:54:53 +0800

    creat BaseViewController

--- ProjectTest1/ProjectTest1/AppDelegate.m
+++ ProjectTest1/ProjectTest1/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "RootViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    RootViewController *rootVC = [[RootViewController alloc] init];
+    UINavigationController *rootNC = [[UINavigationController alloc] initWithRootViewController:rootVC];
+    self.window.rootViewController = rootNC;
+    
     return YES;
 }
 
