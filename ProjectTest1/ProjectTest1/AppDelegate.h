83fce8c8d6cdd7c2ef622163fac4b59fa12a2552
huimai <jason_m@foxmail.com>
2015-12-13 15:31:23 +0800

    creat project

---
+++ ProjectTest1/ProjectTest1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectTest1
+//
+//  Created by a on 15/12/13.
+//  Copyright © 2015年 a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectTest1/ProjectTest1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectTest1
+//
+//  Created by a on 15/12/13.
+//  Copyright © 2015年 a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
