d23d9184ea052bf724bc663fb06fdaa6dff1fa19
XIAOHAN2 <917532427@qq.com>
2015-12-13 17:21:12 +0800

    full

---
+++ ProjectTest1/ProjectTest1/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  ProjectTest1
+//
+//  Created by 蒋潇涵 on 15/12/13.
+//  Copyright © 2015年 a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
